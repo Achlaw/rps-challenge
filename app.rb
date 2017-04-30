067a2cc014f528542660ca807647aaac544f5686
Alex Law <achlawuk@gmail.com>
2017-04-30 16:07:09 +0100

    Add index.erb and play.erb

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+# get '/' do
+#   "This is a form. haha not really but we're getting there."
+# end
+
 get '/' do
-  "This is a form. haha not really but we're getting there."
+  erb(:index)
+end
+
+post '/ready' do
+p params
+@player = params[:player]
+redirect '/play'
+end
+
+get '/play' do
+  erb(:play)
 end
