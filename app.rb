1cc6a5a1e52738646e97afe2aa528a2f8dd36b80
Alex Law <achlawuk@gmail.com>
2017-05-01 01:25:14 +0100

    Attempt so far

--- app.rb
+++ app.rb
@@ -1,20 +1,29 @@
-require 'sinatra'
+require 'sinatra/base'
 
 # get '/' do
 #   "This is a form. haha not really but we're getting there."
 # end
+class RPS < Sinatra::Base
+  get '/' do
+    erb(:index)
+  end
 
-get '/' do
-  erb(:index)
-end
+  post '/ready' do
+    p params
+    @player = params[:player]
+    redirect '/play'
+  end
 
-post '/ready' do
-p params
-player = params[:player]
-@player = player
-redirect '/play'
-end
+  get '/play' do
+    erb(:play)
+  end
 
-get '/play' do
-  erb(:play)
+  get '/select_rock' do
+    redirect '/rock'
+  end
+
+  get '/rock' do
+    @CPU_weapon = ["Rock", "Paper", "Scissors"].sample
+    erb(:rock)
+  end
 end
